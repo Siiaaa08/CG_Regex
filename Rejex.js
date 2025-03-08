69aa65cd6a870b7667afcabbf5ba994d623085d8
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-07 16:42:31 -0800

    [Saloni]Add code of UC2 pincode restricting alphabets

--- Rejex.js
+++ Rejex.js
@@ -1,8 +1,8 @@
 
 function check(pin){
-    const rejex=/^[0-9]{6}$/;
+    const rejex=/^[1-9][0-9]{5}$/;
     return rejex.test(pin)
   }
   
-  console.log(check("228362"));
+  console.log(check("A228362"));
   
