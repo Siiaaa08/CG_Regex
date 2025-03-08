90ceae63c65a4668880a86d4e76a1b694393d1f6
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-07 16:53:04 -0800

    [Saloni]Add code of UC5 email validating email and considering options

--- Rejex.js
+++ Rejex.js
@@ -3,6 +3,11 @@ function check(pin){
     const rejex=/^[1-9][0-9]{2}\s?[0-9]{3}$/ ;
     return rejex.test(pin)
   }
+  function checkEmail(email){
+      //const emailRejex= new RegExp('^[a-zA-Z0-9.]{3,}[a-zA-Z]+@[a-z]{3,}\\.[a-z]{2,}\\.[a-z]{2,}$');
+      const emailRejex=/^[A-Za-z]{3,}(\.[a-zA-Z]*)?\@[a-z]{3,}\.[a-z]{2,}(\.?[a-z]{2,})?$/;
+      return emailRejex.test(email)
+  }
   
-  console.log(check("228362"));
+  console.log(checkEmail("abc.xyz@bridgelabz.co"));
   
