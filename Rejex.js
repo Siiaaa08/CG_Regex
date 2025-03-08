52043e95360b6fbf1bec0ccf8569928c00699191
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-07 16:46:05 -0800

    [Saloni]Add code of UC3 pincode restricting alphabets at last

--- Rejex.js
+++ Rejex.js
@@ -1,8 +1,8 @@
 
 function check(pin){
-    const rejex=/^[1-9][0-9]{5}$/;
+    const rejex=/^[1-9][0-9]{4}[0-9]$/ ;
     return rejex.test(pin)
   }
   
-  console.log(check("A228362"));
+  console.log(check("228362"));
   
